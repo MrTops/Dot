a257c697f17a11fded47503c8e661af9cf28ac1c
MrTops <43430084+MrTops@users.noreply.github.com>
2020-06-07 02:51:42 -0400

    finished bug fixing useStrict and aliases

--- CommandHandler.py
+++ CommandHandler.py
@@ -12,23 +12,24 @@ class CommandHandler(object):
     def hookCommands(self, commandList):
         #hook commands
         for command in commandList:
-            self.hookedCommands[command.callSign] = command
+            self.hookedCommands[command.callSign.lower()] = command
             for alia in command.alias[0]:
-                self.hookedCommands[alia] = command
+                self.hookedCommands[alia.lower()] = command
 
     def postCommand(self, INPUT):
         parsed_INPUT = INPUT.split(' ')
         try:
-            commandFound = self.hookedCommands[parsed_INPUT[0]]
+            commandFound = self.hookedCommands[parsed_INPUT[0].lower()]
             if commandFound.useStrict:
                 if parsed_INPUT[0] == commandFound.callSign:
                     return commandFound.fire(parsed_INPUT) 
                 elif parsed_INPUT[0] in commandFound.alias[0]:
                     return commandFound.fire(parsed_INPUT)
-            else:
+            elif not commandFound.useStrict:
                 if parsed_INPUT[0].lower() == commandFound.callSign.lower():
                     return commandFound.fire(parsed_INPUT) 
                 else:
+                    runCommand = False
                     for alia in commandFound.alias[0]:
                         if parsed_INPUT[0].lower() == alia.lower():
                             runCommand = True
@@ -36,5 +37,7 @@ class CommandHandler(object):
 
                     if runCommand:
                         return commandFound.fire(parsed_INPUT)
+
+            return None
         except Exception:
             return None
