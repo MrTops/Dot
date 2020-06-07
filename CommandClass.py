5ead8e3253fde7340f550ec86c4b0e28b270b6de
MrTops <43430084+MrTops@users.noreply.github.com>
2020-06-07 01:55:50 -0400

    Added useStrict and aliases

--- CommandClass.py
+++ CommandClass.py
@@ -2,9 +2,12 @@ class Command(object):
     """
     The command class describes a command and it's attributes!
     """
-    def __init__(self, callSign, eventHandler):
+    def __init__(self, callSign, eventHandler, useStrict=True, *alias):
         self.callSign = callSign
         self.eventHandler = eventHandler
+        self.alias = alias
+        self.useStrict = useStrict
+        
 
     def fire(self, args):
         return self.eventHandler.callEvent("fired", args)
--- CommandHandler.py
+++ CommandHandler.py
@@ -12,11 +12,25 @@ class CommandHandler(object):
     def hookCommands(self, commandList):
         #hook commands
         for command in commandList:
-            self.hookedCommands[command.callSign] = command
+            if command.useStrict:
+                self.hookedCommands[command.callSign] = command
+                if command.alias:
+                    for alia in command.alias:
+                        self.hookedCommands[alia] = command
+            else:
+                self.hookedCommands[command.callSign.lower()] = command
+                if command.alias:
+                    for alia in command.alias:
+                        self.hookedCommands[alia.lower()] = command
 
     def postCommand(self, INPUT):
         parsed_INPUT = INPUT.split(' ')
         try:
-            return self.hookedCommands[parsed_INPUT[0]].fire(parsed_INPUT)
+            try:
+                commandFound = self.hookedCommands[parsed_INPUT[0].lower()]
+                return commandFound.fire(parsed_INPUT) 
+            except Exception:
+                commandFound = self.hookedCommands[parsed_INPUT[0]]
+                return commandFound.fire(parsed_INPUT)
         except Exception:
             return None
